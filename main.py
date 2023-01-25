d838ac977a3cd2fc77033741b9f2ffad4b630c53
krunalpatel82 <kruhim8280@gmail.com>
2023-01-25 22:24:54 +0100

    write my foundation of my color detection code

---
+++ main.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+
+frameWidth = 640
+frameHeight = 480
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10, 150)
+
+def empty():
+    pass
