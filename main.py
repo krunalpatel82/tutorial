2d61e19fda5ed4e9d4a8ea2beecd246b28887ebb
krunalpatel82 <kruhim8280@gmail.com>
2023-01-27 10:00:00 +0100

    adding line number 14

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ frameHeight = 480
 
 cap = cv2.VideoCapture(0)
 cap1 = cv2.VideoCapture(0)
+cap2= cv2.videoCapture(0)
+cap3= cv2.videoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap.set(10, 150)
